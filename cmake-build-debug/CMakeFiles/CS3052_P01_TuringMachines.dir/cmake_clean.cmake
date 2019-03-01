b5e9dc0d1a9468420e265226aa13fbdb3d909f40
locw <lewysward99@gmail.com>
2019-03-01 15:53:43 +0000

    AInitialised project and added make file

---
+++ cmake-build-debug/CMakeFiles/CS3052_P01_TuringMachines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS3052_P01_TuringMachines.dir/src/main.cpp.o"
+  "CS3052_P01_TuringMachines.pdb"
+  "CS3052_P01_TuringMachines"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS3052_P01_TuringMachines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
